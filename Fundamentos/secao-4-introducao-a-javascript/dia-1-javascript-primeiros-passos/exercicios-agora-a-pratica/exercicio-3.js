822722ee913515725877497c10724880cf2264e5
Rosilene <rosilenefsilva48@gmail.com>
2022-11-29 07:52:14 -0300

    resolvido exercício 3

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-agora-a-pratica/exercicio-3.js
@@ -0,0 +1,13 @@
+// Exercício (3)
+
+const numberOne = 52;
+const numberTwo = 45;
+const numberThree = 74;
+
+if (numberOne > numberTwo && numberOne > numberThree) {
+  console.log(numberOne);  
+}else if(numberTwo > numberOne && numberTwo > numberThree){
+  console.log(numberTwo);
+}else{
+  console.log(numberThree);
+}
