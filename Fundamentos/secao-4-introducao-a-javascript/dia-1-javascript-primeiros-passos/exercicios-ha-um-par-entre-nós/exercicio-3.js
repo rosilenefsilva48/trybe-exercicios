378bd4679dbc7c0fb0a7c6ee1393c4a534aafd7c
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-01 19:19:06 -0300

    resolução do exercício 3

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-ha-um-par-entre-nós/exercicio-3.js
@@ -0,0 +1,13 @@
+//Exercício (3)
+
+
+const costOfProduct = 1;
+const saleValue = 3;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  const totalCostOfProduct = costOfProduct * 1.2;
+  const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
