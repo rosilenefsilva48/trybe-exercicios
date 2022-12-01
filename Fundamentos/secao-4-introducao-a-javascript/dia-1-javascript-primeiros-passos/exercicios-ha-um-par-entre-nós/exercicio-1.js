a327cfbf99685a3ec4aece1ec3170a766eb755db
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-01 10:05:40 -0300

    resolução do exercício 2

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-ha-um-par-entre-nós/exercicio-1.js
@@ -0,0 +1,12 @@
+// Exercício (1)
+
+const number1 = 5;
+const number2 = 3;
+const number3 = 7;
+
+if(number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0){
+  console.log(true);
+}else{
+  console.log(false);
+}
+
