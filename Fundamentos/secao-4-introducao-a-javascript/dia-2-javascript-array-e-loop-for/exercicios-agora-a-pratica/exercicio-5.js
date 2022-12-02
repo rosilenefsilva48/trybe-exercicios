e1a321e72103b29b6057c7348df6567fa07bed93
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-02 19:47:24 -0300

    resolvendo o exercício 5

--- Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/exercicio-5.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/exercicio-5.js
@@ -4,6 +4,17 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let maiorNumero = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+  
+  let numero = numbers[index];
+  
+  if (numero > maiorNumero){
+    maiorNumero = numero
+  }
+}console.log(maiorNumero);
+
 
 
 
