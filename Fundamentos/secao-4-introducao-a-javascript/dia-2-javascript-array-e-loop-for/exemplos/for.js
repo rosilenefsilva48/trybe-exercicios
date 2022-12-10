9c0715e7d03bd3109ad75654c49b76fee2388768
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-09 22:01:29 -0300

    adiciona em exemplos exercicio for

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exemplos/for.js
@@ -0,0 +1,25 @@
+// Anteriormente, para apresentar esses valores, o código que utilizaríamos para executar essa tarefa seria:
+
+/* let cars = ['Saab', 'Volvo', 'BMW'];
+console.log(cars[0]); // Saab
+console.log(cars[1]); // Volvo
+console.log(cars[2]); // BMW
+ */
+
+// Mas, agora que conhecemos o for, podemos melhorar o código para algo como:
+
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]);
+}
+
+// Para fixar, faça o exercício a seguir:
+
+// 1- Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let index = 0; index < groceryList.length; index += 1){
+  console.log(groceryList[index]);
+}
