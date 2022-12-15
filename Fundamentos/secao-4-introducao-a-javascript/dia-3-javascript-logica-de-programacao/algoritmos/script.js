9fa607a2b1da42527132f9eedaa8a8e8726f337a
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-15 09:16:56 -0300

    corrige exemplo em algoritmo

--- Fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao/algoritmos/script.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao/algoritmos/script.js
@@ -1,4 +1,4 @@
-//Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: “valor menor que 16”:o
+//Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: “valor menor que 16”
 
 let fruits = [3, 4, 10, 1, 12];
 let sum = 0; 
