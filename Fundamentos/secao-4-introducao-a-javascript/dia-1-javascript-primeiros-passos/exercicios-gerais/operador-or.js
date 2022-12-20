5102ce01326a27770021357e5ec81771ad607718
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-20 15:42:04 -0300

    exercício or

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicios-gerais/operador-or.js
@@ -0,0 +1,15 @@
+// - Exercício
+
+// - Crie uma variável chamada “weekDay” que receba a string “quarta-feira”.//
+
+let weekDay = 'segunda-feira';
+
+// - Se nossa variável “weekDay” for “segunda-feira”, “terça-feira”, “quarta-feira”, “quinta-feira” ou “sexta-feira”, imprima “Oba, mais um dia de aprendizado na Trybe >:D”.//
+
+// - Se for algum dia de fim de semana, imprima “FINALMENTE, descanso merecido UwU”.//
+
+if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira'){
+  console.log('Oba, mais um dia de aprendizado na Trybe >:D')
+}else{
+  console.log('Finalmente, descanso merecido UwU')
+}
