ccadea72374266c7a387fda80fd8d8d76013ea7e
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-20 14:39:37 -0300

    adiciona exercícios de variáveis

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/variaveis/script.js
@@ -0,0 +1,30 @@
+//Exercíciosq//
+
+// - Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).//
+
+const myName = 'Rose';
+
+// - Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.//
+
+const birthCity = 'Recife';
+
+// - Crie uma variável let chamada birthYear e atribua a ela o ano em que você nasceu.//
+
+let birthYear = 1973;
+
+// - Utilize o console.log() para imprimir as constantes e variáveis que você criou.//
+
+console.log(myName, birthCity, birthYear);
+
+// - Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!//
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+//- Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? //
+
+ birthCity = 'Pernambuco';
+ console.log(birthCity);
+
+ // - O erro ocorre porque eu tentei atribuir um novo valor a uma variável constante. Esse tipo de variável não aceita reatribuição. Ela é uma constante e não pode ser reatribuída.
--- Fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao/exercicios-agora-a-pratica/script.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-3-javascript-logica-de-programacao/exercicios-agora-a-pratica/script.js
@@ -1,4 +1,13 @@
-//Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+// 1- Crie um algoritmo que imprima na tela o fatorial de 10.
+const number = 10
+let fatorialNumber = 1
+
+for(let index = 1; index <= number; index += 1){
+  fatorialNumber = fatorialNumber * index
+}
+console.log(fatorialNumber);
+
+// 2- Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
 let word = 'tryber';
 let newWord = "";
@@ -6,3 +15,29 @@ for(let index = word.length - 1; index >= 0; index -= 1){
   newWord += word[index]
 }
 console.log(`"${newWord}"`);
+
+// 3- Considere o array de strings abaixo:
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// Utilize a estrutura de repetição for para escrever dois algoritmos:
+// um que imprima no console a maior palavra desse array e outro que imprima a menor.
+// Considere o número de caracteres de cada palavra.
+
+let biggestWord = "";
+
+for(let index of array) {
+  if(index.length > biggestWord.length)
+  biggestWord = index
+}
+console.log(biggestWord);
+
+let shortestWord = "";
+for(let index = 0; index < array.length; index += 1){
+  if(index.length < shortestWord.length){
+    shortestWord = index
+  }
+}
+console.log(shortestWord);
+
+//escreva um algoritmo que imprima no console o maior número primo entre 2 e 50.
