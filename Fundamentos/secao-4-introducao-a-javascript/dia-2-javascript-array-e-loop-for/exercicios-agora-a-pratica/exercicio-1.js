ca7bb3a2f75b5717ad68403d12e716f3e7b65178
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-22 09:38:08 -0300

    corrige exercícios agora a prática

--- Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/exercicio-1.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/exercicio-1.js
@@ -4,4 +4,6 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(numbers);
+for(let index of numbers){
+  console.log(index);
+}
--- Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/exercicio-3.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/exercicios-agora-a-pratica/exercicio-3.js
@@ -1,8 +1,8 @@
 // Exercício (3)
 
-/*3. Calcule e imprima a média aritmética dos valores contidos no array;
-
-  * A média aritmética é o resultado da soma de todos os elementos dividido       pelo número total de elementos.
+/*
+Calcule e imprima a média aritmética dos valores contidos no array;
+A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
 */
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
