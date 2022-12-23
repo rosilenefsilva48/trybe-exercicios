c5fe3a9c98bb59fb2fe4b1db95b16d2679a48709
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 11:25:55 -0300

    resolve exercicio 08

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-5-javascript-es6-let-const-arrowfunctions-e-template-literals/exercicio-agora-a-pratica/exercicio08.js
@@ -0,0 +1,20 @@
+/*
+Altere a expressão if/else utilizando ternary operator;
+
+Copie o código abaixo:
+
+let speed = 90;
+const speedCar = (speed) => {
+  if (speed >= 120) {
+    return `Você ultrapassou o limite de velocidade`;
+  } else {
+    return `Você está na velocidade permitida`;
+  }
+};
+console.log(speedCar(speed));
+*/
+let speed = 90;
+const speedCar = (speed) => (
+  speed >= 120 ? `Você ultrapassou o limite de velocidade`: `Você está na velocidade permitida`
+);
+console.log(speedCar(speed));
