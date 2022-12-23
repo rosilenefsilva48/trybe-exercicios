a56823096b79c2ede66fcad4fa127cef88cd61bf
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 11:09:08 -0300

    resolve o exercicio 5

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-5-javascript-es6-let-const-arrowfunctions-e-template-literals/exercicio-agora-a-pratica/exercicio05.js
@@ -0,0 +1,14 @@
+/* Modifique a estrutura das funções a seguir para que elas sejam arrow functions;
+Transforme a função numeroAleatorio em uma arrow function;
+*/
+
+/*function numeroAleatorio() {
+  return Math.random()
+}
+console.log(numeroAleatorio());
+ */
+
+const numeroAleatorio = () => {
+  return Math.random()
+}
+console.log(numeroAleatorio());
