5b99fc62bac3cdc7aee770002cd272b6139a4465
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 08:20:46 -0300

    resolve exercicio 1 de funçao

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/palindromo.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/palindromo.js
@@ -9,3 +9,12 @@ Retorno esperado: true
 verificaPalindrome('desenvolvimento')
 Retorno esperado: false
 */
+function verificaPalindrome(str) {
+  let revert = str.split('').reverse().join('');
+  if(str === revert) {
+    return true;
+  }
+  return false;
+}
+
+console.log(verificaPalindrome('desenvolvimento'));
