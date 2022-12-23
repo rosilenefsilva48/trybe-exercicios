73b6eef82826fcf8e26396c9b8dded39f38be28b
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 08:58:52 -0300

    resolve exercicio 3 funcao

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/menor-valor.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/menor-valor.js
@@ -3,3 +3,13 @@ Crie uma função que receba um array de inteiros e retorne o índice do menor v
 Array de teste: [2, 4, 6, 7, 10, 0, -3];.
 Valor esperado no retorno da função: 6.
 */
+function returnIndexOfArray(arr) {
+  let smallestIndex = 0;
+  for(let index in arr) {
+    if(arr[smallestIndex] > arr[index]) {
+      smallestIndex = index;
+    }    
+  }
+  return smallestIndex;
+}
+console.log(returnIndexOfArray([2, 3, 6, 7, 10, 1]));
