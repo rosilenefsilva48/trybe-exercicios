0f6426be11ce3a47bdc77f3582371bbc3ea29f9f
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 11:59:37 -0300

    criando arquivos dos exercicios

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-5-javascript-es6-let-const-arrowfunctions-e-template-literals/exercicios-mais-pratica/exercicio03.js
@@ -0,0 +1,11 @@
+/*
+ Crie uma função que receba uma frase como parâmetro e retorne a maior palavra da frase.
+
+Exemplo:
+
+Copiar
+  longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+De olho na dica: Nesse exercício, será necessário utilizar os conhecimentos sobre array, método split e loop for/of.
+
+Bônus (opcional): Tente fazer o mesmo exercício utilizando o método array.sort().
+*/
