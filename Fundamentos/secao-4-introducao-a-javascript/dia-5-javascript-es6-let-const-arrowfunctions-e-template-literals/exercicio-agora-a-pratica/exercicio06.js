ce3d47adf8ab63e3a2fded301868c119d6550330
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 11:13:14 -0300

    resolve exercicio 06

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-5-javascript-es6-let-const-arrowfunctions-e-template-literals/exercicio-agora-a-pratica/exercicio06.js
@@ -0,0 +1,16 @@
+/*
+Transforme a função hello em uma arrow function;
+Copie o código abaixo:
+
+  function hello(nome) {
+    return `Olá, ${nome}!`
+  }
+  let nome = 'Ivan';
+  console.log(hello(nome));
+*/
+
+const hello = (nome) => {
+  return `Olá, ${nome}!`
+}
+let nome = 'Ivan';
+console.log(hello(nome));
