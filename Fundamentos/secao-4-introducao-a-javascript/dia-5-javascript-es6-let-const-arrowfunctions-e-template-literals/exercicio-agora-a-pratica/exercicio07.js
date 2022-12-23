2e3f000668f030e80f4fb45ce8cee3ceec83497f
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 11:16:00 -0300

    resolve exercicico 07

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-5-javascript-es6-let-const-arrowfunctions-e-template-literals/exercicio-agora-a-pratica/exercicio07.js
@@ -0,0 +1,18 @@
+/*
+ Transforme a função nomeCompleto em uma arrow function;
+Copie o código abaixo:
+
+function nomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`
+}
+let nome = 'Ivan';
+let sobrenome = 'Pires';
+console.log(nomeCompleto(nome, sobrenome));
+*/
+
+const nomeCompleto = (nome, sobrenome) => {
+  return `${nome} ${sobrenome}`
+}
+let nome = 'Ivan';
+let sobrenome = 'Pires';
+console.log(nomeCompleto(nome, sobrenome));
