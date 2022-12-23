64eac39537ffef34338f4d9a42abf863e69cdd5b
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 09:16:51 -0300

    resolve exercicio 4 de funcao

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/maior-nome.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/maior-nome.js
@@ -5,3 +5,13 @@ Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
 
 Valor esperado no retorno da função: Fernanda.
 */
+function findBiggestName(names) {
+  let biggestName = names[0];
+  for(let index in names) {
+    if(biggestName.length < names[index].length) {
+      biggestName = names[index];
+    }
+  }
+  return biggestName
+}
+console.log(findBiggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
