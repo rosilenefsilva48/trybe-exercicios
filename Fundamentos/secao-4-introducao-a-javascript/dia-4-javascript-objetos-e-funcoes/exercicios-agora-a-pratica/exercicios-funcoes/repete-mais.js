93d085a5b437a6ccb9953a31ce1faed26001fa8f
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 09:48:20 -0300

    resolve o exercicio 5 de funcoes

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/repete-mais.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/repete-mais.js
@@ -5,3 +5,25 @@ Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 
 Valor esperado no retorno da função: 2.
 */
+function mostRepeated(integers) {
+  
+  let mostRepeatedCounter = 0;
+  let actualNumberCounter = 0;
+  let mostRepeatedIndex = 0;
+
+  for(let index in integers) {
+    let checkNumber = integers[index];
+    for(let secondIndex in integers) {
+      if(checkNumber === integers[secondIndex]) {
+        actualNumberCounter += 1;
+      }
+    }
+    if(actualNumberCounter > mostRepeatedCounter) {
+      mostRepeatedCounter = actualNumberCounter;
+      mostRepeatedIndex = index;
+    }
+    actualNumberCounter = 0;
+  }
+  return integers[mostRepeatedIndex];
+}
+console.log(mostRepeated([2, 3, 2, 5, 8, 2, 3]));
