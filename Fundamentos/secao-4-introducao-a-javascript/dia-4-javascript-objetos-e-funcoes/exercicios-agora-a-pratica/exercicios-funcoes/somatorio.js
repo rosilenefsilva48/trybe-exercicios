2c2d412325816a0cce08f1cb22f7153ed26f56cf
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-23 09:56:53 -0300

    resolve exercicio 6 funcoes

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/somatorio.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercicios-agora-a-pratica/exercicios-funcoes/somatorio.js
@@ -5,3 +5,12 @@ Valor de teste: N = 5.
 
 Valor esperado no retorno da função: 1+2+3+4+5 = 15.
 */
+
+function summation(number) {
+  let result = 0;
+  for(let index = 1; index <= number; index +=1 ) {
+    result += index;
+  }
+  return result;
+}
+console.log(summation(5));
