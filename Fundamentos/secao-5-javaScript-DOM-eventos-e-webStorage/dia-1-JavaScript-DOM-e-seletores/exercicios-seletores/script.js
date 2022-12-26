4d27c0e425192772314b35b34f9f559e57058165
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-26 14:40:25 -0300

    resolve exercicio de administrador de tempo

--- Fundamentos/secao-5-javaScript-DOM-eventos-e-webStorage/dia-1-JavaScript-DOM-e-seletores/exercicios-seletores/script.js
+++ Fundamentos/secao-5-javaScript-DOM-eventos-e-webStorage/dia-1-JavaScript-DOM-e-seletores/exercicios-seletores/script.js
@@ -1,6 +1,21 @@
 const header = document.getElementById("header-container");
-header.style.backgroundColor = 'green'
 
-const section1 = document.getElementsByClassName(".emergency-tasks");
+const section1 = document.getElementsByClassName("emergency-tasks");
 
-section1.style.backgroundColor = 'orange'
+const section2 = document.getElementsByClassName("no-emergency-tasks");
+
+const h3Emergency = document.querySelectorAll('.emergency-tasks div h3');
+
+const h3NoEmergency = document.querySelectorAll('.no-emergency-tasks div h3');
+
+const footer = document.getElementById("footer-container");
+
+
+header.style.backgroundColor = 'rgb(0, 176, 105)';
+section1[0].style.backgroundColor = 'rgb(255, 159, 132)';
+section2[0].style.backgroundColor = 'rgb(249, 219, 94)';
+h3Emergency[0].style.backgroundColor = 'rgb(165, 0, 243)';
+h3Emergency[1].style.backgroundColor = 'rgb(165, 0, 243)';
+h3NoEmergency[0].style.backgroundColor = 'rgb(35, 37, 37)';
+h3NoEmergency[1].style.backgroundColor = 'rgb(35, 37, 37)';
+footer.style.backgroundColor = "rgb(0, 53, 51)";
