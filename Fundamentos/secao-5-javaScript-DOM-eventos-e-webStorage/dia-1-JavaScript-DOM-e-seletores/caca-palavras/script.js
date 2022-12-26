abbfbfb0ec7e6f00dc4cfc72a544c64376bb85fc
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2022-12-26 18:36:12 -0300

    continuação de caçapalavras

--- Fundamentos/secao-5-javaScript-DOM-eventos-e-webStorage/dia-1-JavaScript-DOM-e-seletores/caca-palavras/script.js
+++ Fundamentos/secao-5-javaScript-DOM-eventos-e-webStorage/dia-1-JavaScript-DOM-e-seletores/caca-palavras/script.js
@@ -1,5 +1,37 @@
+const arrayWords = ['LARANJA', 'PERA', 'LARANJA', 'UVA', 'MANGA']
+
+// função para gerar células dento da matriz
 const generateCells = () => {
   const matriz = document.querySelector('.matriz');
-  console.log(matriz);
+  for(let index = 0; index < 10; index +=1){
+    const line = document.createElement('div');
+    line.className = 'line';
+    for(let index1 = 0; index1 < 10; index1 +=1){
+      const cell = document.createElement('div');
+      cell.className = 'cell';
+      line.appendChild(cell)
+    }
+    matriz.appendChild(line)
+  }
 }
 generateCells();
+
+const addWords = () => {
+  const lines = document.querySelectorAll('.line');
+  const indexLine = Math.floor(Math.random() * lines.length);
+  
+  const indexWord = Math.floor(Math.randon() * arrayWords.length);
+  const word = arrayWords[indexWord];
+
+  const cells = lines[indexLine].querySelectorAll('.cell');
+  const indexCell = Math.floor(Math.randon() * (cells.length - word.length + 1));
+  
+  for(let index = 0; index < word.length; index += 1){
+    cells[indexCell + index].innerText = word[index];
+  }
+}
+
+for(let index = 0; index < arrayWords.length; index += 1){
+  addWords();
+}
+addWords();
