d3ec64ee19a26579826e8c5ce89c675ab00b4440
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-01-19 10:31:16 -0300

    resolve exercicio pedido de cliente

--- Fundamentos/secao-6-intro-javascript-es6-e-testes-unitarios/exercicios-pedido-de-cliente/script.js
+++ Fundamentos/secao-6-intro-javascript-es6-e-testes-unitarios/exercicios-pedido-de-cliente/script.js
@@ -36,14 +36,14 @@ const order = {
 
 const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  return `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street} ${order.address.number}, AP: ${order.address.apartment}`
 };
-
 customerInfo(order);
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  order.name = 'Luiz Silva'
+  order.payment.total = '50.00'
+  return `Olá, ${order.name}, o total do seu pedido de marguerita, pepperoni e Coca-cola Zero é R$${order.payment.total}`
 };
-
 orderModifier(order);
