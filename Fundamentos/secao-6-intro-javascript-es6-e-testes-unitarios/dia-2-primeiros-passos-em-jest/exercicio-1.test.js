8156e7ffc23acf9cebc4e0dde66cec4ea5fd3b69
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-01-20 10:53:42 -0300

    resolve exercicic 1

---
+++ Fundamentos/secao-6-intro-javascript-es6-e-testes-unitarios/dia-2-primeiros-passos-em-jest/exercicio-1.test.js
@@ -0,0 +1,16 @@
+const myRemove = require('./exercicio-1')
+
+describe('exercicio 1', () => {
+  
+  test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado;', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);    
+  });
+  
+  test('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  
+  test('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
