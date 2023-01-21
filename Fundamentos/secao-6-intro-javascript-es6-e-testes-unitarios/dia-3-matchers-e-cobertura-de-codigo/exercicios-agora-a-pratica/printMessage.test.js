5c674563abacfd6f2bf2d133cbc40a052b4f0a7c
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-01-21 18:15:34 -0300

    resolve exercicio 3

--- Fundamentos/secao-6-intro-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercicios-agora-a-pratica/printMessage.test.js
+++ Fundamentos/secao-6-intro-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercicios-agora-a-pratica/printMessage.test.js
@@ -5,4 +5,12 @@ describe('testes para a função printMessage', () => {
   test('Verifica se o objeto passado como parâmetro possui a propriedade "personagem"', () => {
     expect(info).toHaveProperty('personagem');
   });
+
+  test('Verifica se a resposta contém a informação "Boas vindas," antes de chamar o nome da personagem', () => {
+    expect(printMessage(info)).toMatch('Boas vindas');
+  });
+
+  test('Verifica se a resposta contém o nome correto da personagem', () => {
+    expect(printMessage(info)).toMatch('Margarida');
+  });
 });
