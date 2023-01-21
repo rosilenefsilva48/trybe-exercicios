725434cbba109aba226d43cc8a3b808c811fef9d
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-01-21 10:35:19 -0300

    resolve exercicios de fixaçaõ

--- Fundamentos/secao-6-intro-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercicios-de-fixacao/createItem.test.js
+++ Fundamentos/secao-6-intro-javascript-es6-e-testes-unitarios/dia-3-matchers-e-cobertura-de-codigo/exercicios-de-fixacao/createItem.test.js
@@ -6,23 +6,23 @@ describe('a função createItem', () => {
     expect(createItem('banana', 'kg', 1.99, 20)).toEqual({ name: 'banana', unit: 'kg', price: 1.99, quantity: 20 })
   });
   
-  /* it('utiliza zero como quantidade padrão', () => {
-    expect()
+  it('utiliza zero como quantidade padrão', () => {
+    expect(createItem('banana', 'kg', 1.99)).toEqual({name:'banana', unit: 'kg', price: 1.99, quantity: 0})
   });
   
-  it('Lança um erro quando não recebe parâmetros', () => {
-    expect()
+   it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => { createItem() }).toThrow();
   });
   
   it('Lança um erro se o nome do item não é uma string', () => {
-
+    expect(() => { createItem(1.99, 'kg', 20) }).toThrow(new Error('O nome do item deve ser uma string'));
   });
   
   it('Lança um erro se o preço é negativo', () => {
-
+    expect(() => { createItem('banana', 'kg', -1.99, 20) }).toThrow(new Error('O preço do item deve ser maior que zero'))
   });
   
-  it('Lança um erro se o preço é zero'), () => {
-
-  }; */
+  it('Lança um erro se o preço é zero', () => {
+    expect(() => { createItem('banana', 'kg', 0, 20) }).toThrow(new Error('O preço do item deve ser maior que zero'))
+  });  
 });
