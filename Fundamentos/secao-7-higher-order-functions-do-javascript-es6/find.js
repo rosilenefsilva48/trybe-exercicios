e7c14b09894b1cde1dbb07499f9e66f1c337531e
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-01-25 16:18:47 -0300

    resolve outro exercicio find

--- Fundamentos/secao-7-higher-order-functions-do-javascript-es6/find.js
+++ Fundamentos/secao-7-higher-order-functions-do-javascript-es6/find.js
@@ -1,4 +1,4 @@
-//Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+// 1. Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
 
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
@@ -6,3 +6,21 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 const verifyNumbers = numbers.find((number) => number % 3 === 0 && number % 5 === 0);
 
 console.log(verifyNumbers);
+
+// 2. Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const fiveLettersName = names.find((name) => name.length === 5);
+
+console.log(fiveLettersName);
+
+// 3. Utilize o find para encontrar a música com id igual a '31031685', caso ela exista:
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+
+const verifyID = musicas.find((music) => music.id === '31031685');
+
+console.log(verifyID);
