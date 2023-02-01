0083e4b6f7b924530a7b9826518d37911e2c5adf
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-02-01 15:08:38 -0300

    dia 2 exercicios para fixar

---
+++ Fundamentos/secao-7-higher-order-functions-do-javascript-es6/dia-2-higher-order-function-sort-map/exercicio1.js
@@ -0,0 +1,12 @@
+const books = require('./data-books');
+
+// Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+
+const formatedBookNames = () => {
+  return books.map((book) => (`${book.name} - ${book.genre} - ${book.author.name}`));
+} < arr++) {
+  const eleme];
+  
+}
+
+//Crie um array com strings no formato NOME_DA_PESSOA_AUTORA - DATA DE NASCIMENTO
