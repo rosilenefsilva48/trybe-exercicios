0b3ccc37ac1f9a56f5f07fec716d0b647f11041b
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-02-01 17:55:11 -0300

    resolve desafio pratico

---
+++ Fundamentos/secao-7-higher-order-functions-do-javascript-es6/dia-3-higher-order-functions/exercicio-agora-a-pratica/organizando-biblioteca/exercicio2.js
@@ -0,0 +1,73 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// 2 - Filtre os livros com mais de 60 anos desde sua publicação e ordene a partir do livro mais velho para o mais novo.
+
+const actualDate = new Date();
+const actualYear = actualDate.getFullYear();
+
+//De olho na dica 👀: para acessar o ano atual, pesquise sobre “como resgatar o ano atual com JavaScript” e use as funções filter e sort.
+
+const filterBooks = books.filter((book) => (actualYear - book.releaseYear) >= 60).sort((a, b) => a.releaseYear - b.releaseYear);
+
+console.log(filterBooks);
