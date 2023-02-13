d936430f247c1483d5c5daf83609b4475f2b8e09
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-02-13 11:12:44 -0300

    exercicio de fixação

---
+++ Front-End/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-2-javascript-assincrono-promisses-e-fetch/exercicio-de-fixacao/index.js
@@ -0,0 +1,46 @@
+import './style.css';
+
+import {
+  fillUsersSelect,
+  fillPosts,
+  fillFeaturedPostComments,
+  clearPageData,
+  fillErrorMessage,
+} from './utils/updateUI';
+
+const usersSelect = document.querySelector('#users-select');
+
+const USERS_API = 'https://dummyjson.com/users';
+fetch(USERS_API)
+  .then((response) => response.json())
+  .then((data) => {
+    const { users } = data;
+    fillUsersSelect(users);
+  });
+
+usersSelect.addEventListener('change', () => {
+  clearPageData();
+
+  const POST_API = `https://dummyjson.com/posts/user/${userID}`;
+
+  fetch(POST_API)
+    .then((response) => response.json())
+    .then((data) => {
+      const { posts } = data;
+      fillPosts(posts);
+
+      const [featuredPost] = posts;
+      const COMMENTS_API = `https://dummyjson.com/posts/${featuredPost.id}/comments`;
+
+      return fetch(COMMENTS_API);
+    })
+    .then((res) => res.json)
+    .then((data) => {
+      const { comments } = data;
+      fillFeaturedPostComments(comments);
+    })
+    .catch((error) => {
+      fillErrorMessage('Erro ao recuperar informações');
+      console.log(error.message);
+    });
+});
