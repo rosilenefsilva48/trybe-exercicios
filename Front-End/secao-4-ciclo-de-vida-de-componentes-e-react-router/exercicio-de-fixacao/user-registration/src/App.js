09e1b69e353a014f0119447c15250182df543341
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-03-10 11:34:54 -0300

    Resolve exercicio de fixação

---
+++ Front-End/secao-4-ciclo-de-vida-de-componentes-e-react-router/exercicio-de-fixacao/user-registration/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PersonDetails from './components/PersonDetails';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PersonDetails />
+      </div>
+    );
+  }
+}
+
+export default App;
