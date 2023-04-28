37a0d4665048b783cc3ce998e2475085e6dede76
rosilenefsilva48 <rosilenefsilva48@gmail.com>
2023-04-28 09:57:25 -0300

    resolve exercicio agora a pratica

---
+++ Front-End/secao-9-context-api-e-react-hooks/dia-1-react-hooks-usestate-e-useefect/exercicio-agora-a-pratica/trybemail/src/data/messagesList.js
@@ -0,0 +1,18 @@
+const messagesList = [
+  {
+    id: 1,
+    title: 'Olá! Tenho uma vaga que bate com seu perfil, podemos marcar uma conversa?',
+    status: 0,
+  },
+  {
+    id: 2,
+    title: 'Parabéns! Você avançou no processo da vaga Pessoa Desenvolvedora',
+    staus: 0,
+  },
+  {
+    id: 3,
+    title: 'RE: Entrevista marcada',
+  },
+];
+
+export default messagesList;
